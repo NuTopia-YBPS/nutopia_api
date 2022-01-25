8c3182161d66843bc28186df64fb1a800700f343
= <marudhupaandian@gmail.com>
2022-01-25 12:31:00 +0530

    cleaned up naming

--- nutopia_api/urls.py
+++ nutopia_api/urls.py
@@ -1,4 +1,4 @@
-"""mailer_api URL Configuration
+"""nutopia_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
+
 from django.contrib import admin
 from django.urls import path, include
 
--- nutopia_api/wsgi.py
+++ nutopia_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mailer_api project.
+WSGI config for nutopia_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mailer_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutopia_api.settings')
 
 application = get_wsgi_application()
